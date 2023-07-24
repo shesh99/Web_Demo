be963ba8dcd99524ea3e8fd89ec2280a022f61ce
shesh99 <sheshandilanka99@gmail.com>
2023-07-24 11:34:20 +0530

    VIHANGA

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log("hi icet...");
+
+
+var name = "yasindu"
+
+console.log(name+" Is my name");
+
+function print(){
+    console.log("print called");
+}
+
+print();
+
+function getdata(){
+    var email = document.getElementById('email').value
+    console.log(email);
+}
---
+++ js/vihanga.js
@@ -0,0 +1,19 @@
+let username;
+letpassword;
+
+console.log("ICET");
+sum=2+2;
+console.log(sum);
+console.log("2");
+console.log("2");
+console.log("3");
+    function sampleOne(){
+        let sum=2+2;
+        console.log(sum)
+    }
+    function sampleTwo(){
+        let sum=4*4
+        console.log(sum);
+    }
+console.log("4*4");
+function sampletwo(){}
